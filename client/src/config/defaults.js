1cbf59b5c46564d23dbc348a03f8ff3d5dafbe6a
Aman Pandey <ap.pandeyaman@gmail.com>
2026-05-10 19:12:26 +0530

    Update

--- client/src/config/defaults.js
+++ client/src/config/defaults.js
@@ -1,3 +1,3 @@
-export const BACKEND_HOST_URL = "http://localhost:5000"
+export const BACKEND_HOST_URL = "https://blog-website-project-87wy.onrender.com"
 export const GOOGLE_CLIENT_ID = "757225468923-skqe5bn3so9broj5g7gfe48i0efpvhkj.apps.googleusercontent.com"
 export const CLOUDINARY_USER_NAME = "dwlk30o8b";
